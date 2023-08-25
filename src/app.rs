ae8564c8c316ec7497af47536b5253eb8cace1de
Mustafa Zaki Assagaf <mustafa.segf@gmail.com>
2023-08-25 18:15:25 +0700

    feat: added the rest of the ui

--- src/app.rs
+++ src/app.rs
@@ -1,6 +1,5 @@
 use std::error;
 
-use cursorvec::CursorVec;
 use tui_tree_widget::TreeItem;
 
 use crate::items::{Item, StatefulTree};
@@ -12,9 +11,21 @@ pub type AppResult<T> = std::result::Result<T, Box<dyn error::Error>>;
 #[derive(Debug)]
 pub struct App {
     pub running: bool,
-    pub counter: u8,
     pub sidebar: SideBar,
     pub settings: Settings,
+    pub tabs: TabBar,
+    pub urlbar: UrlBar,
+    pub requestbar: RequestBar,
+}
+
+#[derive(Debug, Default)]
+pub enum Selected {
+    #[default]
+    Sidebar,
+    Tabs,
+    MethodBar,
+    Urlbar,
+    Requestbar,
 }
 
 #[derive(Debug)]
@@ -24,10 +35,73 @@ pub struct SideBar {
     pub tree: StatefulTree<'static>,
 }
 
+#[derive(Debug, Default)]
+pub struct TabBar {
+    pub selected: usize,
+    pub tabs: Vec<Item>,
+}
+
+
+#[derive(Debug, Default)]
+pub enum InputMode {
+    #[default]
+    Normal,
+    Insert,
+}
+
+#[derive(Debug, Default, strum::Display)]
+pub enum Method {
+    #[default]
+    Get,
+    Post,
+    Put,
+    Patch,
+    Delete,
+    Head,
+    Options,
+    Other(String),
+}
+
+
+
+#[derive(Debug, Default)]
+pub struct UrlBar {
+    pub title: String,
+    pub text: String,
+    pub cursor_position: usize,
+    pub input_mode: InputMode,
+    pub method: Method,
+}
+
+#[derive(Debug, Default, strum::Display, strum::EnumIter)]
+pub enum RequestMenu {
+    #[default]
+    Params,
+    Authentication,
+    Headers,
+    Body,
+}
+
+
+#[derive(Debug, Default)]
+pub struct RequestBar {
+    pub body: String,
+    pub request_menu: RequestMenu,
+}
+
 #[derive(Debug)]
 pub struct Settings {
     pub show_sidebar: bool,
     pub show_help: bool,
+}
+
+impl Default for Settings {
+    fn default() -> Self {
+        Self {
+            show_sidebar: true,
+            show_help: false,
+        }
+    }
 }
 
 impl Default for App {
@@ -51,17 +125,31 @@ impl Default for App {
             TreeItem::new_leaf(Item::new("d")),
         ]);
         tree.first();
+
+        let tabs = tree.items.iter().map(|i| i.inner().clone()).collect();
+
         Self {
             running: true,
-            counter: 0,
             sidebar: SideBar {
-                size: 45,
+                size: 25,
                 selected: 0,
                 tree,
             },
             settings: Settings {
                 show_sidebar: true,
                 show_help: false,
+            },
+            tabs: TabBar { selected: 0, tabs },
+            urlbar: UrlBar {
+                title: String::from("localhost:8080"),
+                text: String::from("localhost:8080"),
+                cursor_position: 0,
+                input_mode: InputMode::Normal,
+                method: Method::Get,
+            },
+            requestbar: RequestBar {
+                body: String::new(),
+                request_menu: RequestMenu::Params,
             },
         }
     }
--- src/ui.rs
+++ src/ui.rs
@@ -1,10 +1,11 @@
 use ratatui::{
     prelude::*,
-    widgets::{Block, BorderType, Borders, Paragraph},
+    widgets::{Block, BorderType, Borders, Paragraph, Tabs, Wrap},
 };
+use strum::IntoEnumIterator;
 use tui_tree_widget::Tree;
 
-use crate::app::App;
+use crate::app::{App, RequestMenu};
 
 /// Renders the user interface widgets.
 pub fn render<B: Backend>(app: &mut App, frame: &mut Frame<'_, B>) {
@@ -40,15 +41,6 @@ pub fn render<B: Backend>(app: &mut App, frame: &mut Frame<'_, B>) {
 }
 
 pub fn sidebar<B: Backend>(app: &mut App, frame: &mut Frame<'_, B>, area: Rect) {
-    // let paragraph = Paragraph::new(app.sidebar.items.print_item(0)).block(
-    //     Block::default()
-    //         .title("Sidebar")
-    //         .borders(Borders::ALL)
-    //         .border_type(BorderType::Rounded),
-    // );
-
-    // frame.render_widget(paragraph, area);
-
     let indicies = app.sidebar.tree.state.selected();
     let item = app.sidebar.tree.items.get(indicies[0]);
 
@@ -56,26 +48,131 @@ pub fn sidebar<B: Backend>(app: &mut App, frame: &mut Frame<'_, B>, area: Rect) 
         item.and_then(|item| item.children().get(i))
     });
 
+    let block = Block::default()
+        .borders(Borders::ALL)
+        .border_type(BorderType::Rounded)
+        .title(format!(
+            "{:?}",
+            selected.map(|item| item.inner().to_string())
+        ));
+
     let items = Tree::new(app.sidebar.tree.items.clone())
-        .block(
-            Block::default()
-                .borders(Borders::ALL)
-                .title(format!("{:?}", selected.map(|item| item.inner().to_string()))),
-        )
+        .block(block)
         .highlight_style(
             Style::default()
                 .fg(Color::LightBlue)
-                // .bg(Color::LightGreen)
                 .add_modifier(Modifier::BOLD)
                 .add_modifier(Modifier::UNDERLINED),
         );
     frame.render_stateful_widget(items, area, &mut app.sidebar.tree.state);
 }
 
-pub fn mainbar<B: Backend>(_app: &mut App, frame: &mut Frame<'_, B>, area: Rect) {
+pub fn mainbar<B: Backend>(app: &mut App, frame: &mut Frame<'_, B>, area: Rect) {
+    let chunks = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints(
+            [
+                Constraint::Min(3),
+                Constraint::Min(3),
+                Constraint::Ratio(1, 2),
+                Constraint::Ratio(1, 2),
+            ]
+            .as_ref(),
+        )
+        .split(area);
+
     let block = Block::default()
-        .title("Mainbar")
+        .title("Reponse")
         .borders(Borders::ALL)
         .border_type(BorderType::Rounded);
-    frame.render_widget(block, area);
+
+    tabs(app, frame, chunks[0]);
+    urlbar(app, frame, chunks[1]);
+    requestbar(app, frame, chunks[2]);
+    frame.render_widget(block, chunks[3]);
+}
+
+pub fn tabs<B: Backend>(app: &mut App, frame: &mut Frame<'_, B>, area: Rect) {
+    let titles = app
+        .tabs
+        .tabs
+        .iter()
+        .map(|item| Line::from(item.to_string()))
+        .collect();
+
+    let tabs = Tabs::new(titles)
+        .block(Block::default().borders(Borders::ALL).title("Tabs"))
+        .select(app.tabs.selected)
+        .style(Style::default().fg(Color::Cyan))
+        .highlight_style(
+            Style::default()
+                .add_modifier(Modifier::BOLD)
+                .bg(Color::Black),
+        );
+
+    frame.render_widget(tabs, area);
+}
+
+pub fn urlbar<B: Backend>(app: &mut App, frame: &mut Frame<'_, B>, area: Rect) {
+    let chunks = Layout::default()
+        .direction(Direction::Horizontal)
+        .constraints([Constraint::Min(10), Constraint::Min(0)].as_ref())
+        .split(area);
+
+    let block = Block::default()
+        .title(format!("URL: {}", app.urlbar.title))
+        .borders(Borders::ALL)
+        .border_type(BorderType::Rounded);
+
+    let text = Paragraph::new(app.urlbar.text.clone())
+        .block(block)
+        .wrap(Wrap { trim: true })
+        .alignment(Alignment::Left);
+
+    let method_block = Block::default()
+        .borders(Borders::ALL)
+        .border_type(BorderType::Rounded);
+
+    let method = Paragraph::new(app.urlbar.method.to_string())
+        .block(method_block)
+        .wrap(Wrap { trim: true })
+        .alignment(Alignment::Left);
+
+    frame.render_widget(method, chunks[0]);
+    frame.render_widget(text, chunks[1]);
+}
+
+pub fn requestbar<B: Backend>(app: &mut App, frame: &mut Frame<'_, B>, area: Rect) {
+    let chunks = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints([Constraint::Min(1), Constraint::Min(0)].as_ref())
+        .split(area);
+
+    let titles = RequestMenu::iter()
+        .map(|item| Line::from(item.to_string()))
+        .collect();
+
+    let tabs = Tabs::new(titles)
+        // .block(Block::default())
+        .select(0)
+        .style(Style::default().fg(Color::Cyan))
+        .highlight_style(
+            Style::default()
+                .add_modifier(Modifier::BOLD)
+                .bg(Color::Black),
+        );
+
+    frame.render_widget(tabs, chunks[0]);
+
+    let block = Block::default()
+        .title("request")
+        .borders(Borders::ALL)
+        .border_type(BorderType::Rounded);
+
+    let text = Paragraph::new(app.requestbar.body.clone())
+        .block(block)
+        .wrap(Wrap { trim: true })
+        .alignment(Alignment::Left);
+
+    frame.render_widget(text, chunks[1]);
 }
